b0b4db2e44bcd668d079aec232cb8a49badbc8f2
Nikita Alutis <nikalutis@yandex.ru>
2023-02-22 05:59:26 +0300

    l8

--- api/main.py
+++ api/main.py
@@ -7,19 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    fo= open("/tmp/test.txt", "w")
-    filebuffer = ["brave new world"]
-    fo.writelines(filebuffer)
+    fo= open("/tmp/back.txt", "r")
+    message = fo.readline()
     fo.close()
-    message = ''
-    with open('data.json', 'r') as f:
-        message = "TADAA"
     if flask.request.method == 'POST':
         print("heyheyheyhey")
         # message = "!!"
-        fo= open("/tmp/test.txt", "r")
-        message = fo.readline()
+        fo= open("/tmp/back.txt", "r")
+        filebuffer = [flask.request.form['name-input']]
+        fo.writelines(filebuffer)
         fo.close()
+
         # message = 'Hello ' + flask.request.form['name-input'] + '!'
 
     return flask.render_template('index.html', message=message)
