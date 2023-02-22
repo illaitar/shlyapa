0b41a553e5f1e6832899baaeb59c86084d72affa
Nikita Alutis <nikalutis@yandex.ru>
2023-02-22 05:17:08 +0300

    kol

---
+++ app.py
@@ -0,0 +1,24 @@
+import flask
+import json 
+from app import views
+from flask import Flask
+
+# A simple Flask App which takes
+# a user's name as input and responds
+# with "Hello {name}!"
+app = Flask(__name__)
+
+app = flask.Flask(__name__)
+
+app.config['SERVER_NAME'] = 'bruh'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ''
+    if flask.request.method == 'POST':
+        message = 'Hello ' + flask.request.form['name-input'] + '!'
+        import json
+        with open('data.json', 'w') as f:
+            json.dump({}, f)
+    return flask.render_template('index.html', message=message)
+
