bbbf5b457d53e797b0119b7df4ec389bf52e8e87
Nikita Alutis <nikalutis@yandex.ru>
2023-02-22 05:01:10 +0300

    1

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from flask import request
+
+@app.errorhandler(Exception)
+def handle_exception(err):
+  path = request.path # this var was shown to be 'favicon.ico' or 'manifest.json'
+  
 @app.route('/')
 def hello_world(): 
     return 'Hello World!'
