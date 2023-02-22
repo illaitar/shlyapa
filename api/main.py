ed5ab7b0f766aefb7f6d967cc4f5dc6f91c91844
Nikita Alutis <nikalutis@yandex.ru>
2023-02-22 06:15:20 +0300

    l13

--- api/main.py
+++ api/main.py
@@ -9,22 +9,24 @@ app = Flask(__name__)
 
 @app.route('/cards', methods=['GET', 'POST'])
 def home():
-    if os.path.isfile("/tmp/back.txt"):
-        fo= open("/tmp/back.txt", "r")
-        message = fo.readline()
-        fo.close()
-    else:
-        message = ''
     if flask.request.method == 'POST':
-        print("heyheyheyhey")
-        # message = "!!"
-        fo= open("/tmp/back.txt", "w")
-        nm = str(flask.request.form['name-input'])
-        filebuffer = [nm]
-        fo.writelines(filebuffer)
-        fo.close()
+        code = flask.request.form['code']
+        cards = [flask.request.form[f"card{i}"] for i in range(1,6)]
+        current = []
+        if os.path.isfile(f"/tmp/{code}.txt"):
+            fo = open("/tmp/back.txt", "r")
+            current = fo.readlines()
+            fo.close()
+        for card in cards:
+            current.append(card)
+        
 
+        # message = "!!"
+        fo = open("/tmp/back.txt", "w")
+        fo.writelines(current)
+        fo.close()
+        message = "Submitted Cards!"
         # message = 'Hello ' + flask.request.form['name-input'] + '!'
 
-    return flask.render_template('index.html', message=message)
+    return flask.render_template('index.html', message="")
 
