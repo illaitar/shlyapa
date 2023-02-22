f1e37672e2c4f4087eed51b9f468cc3e2de7466b
Nikita Alutis <nikalutis@yandex.ru>
2023-02-22 05:45:50 +0300

    l2

--- api/main.py
+++ api/main.py
@@ -8,11 +8,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     message = ''
+    with open('data.json', 'w') as f:
+        message = "TADAA"
     if flask.request.method == 'POST':
         print("heyheyheyhey")
         # message = "!!"
         message = 'Hello ' + flask.request.form['name-input'] + '!'
-        with open('data.json', 'w') as f:
-            message = "TADAA"
+
     return flask.render_template('index.html', message=message)
 
