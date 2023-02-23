be625d16403e584a925210d68c4876fca1ca1730
Nikita Alutis <nikalutis@yandex.ru>
2023-02-23 04:03:34 +0300

    fully working

--- api/main.py
+++ api/main.py
@@ -3,10 +3,11 @@ import json
 from flask import Flask
 import os.path
 from random import shuffle
+import numpy as np
 
 
 app = Flask(__name__)
-
+app.static_folder = 'static'
 
 @app.route('/cards', methods=['GET', 'POST'])
 def home():
@@ -28,7 +29,7 @@ def home():
         fo = open(f"/tmp/{code}.txt", "w")
         fo.writelines(current)
         fo.close()
-        message = "Submitted Cards!"
+        message = "Карты отправлены!"
         # message = 'Hello ' + flask.request.form['name-input'] + '!'
         not_entered_cards = False
     return flask.render_template('index.html', message=message, not_entered_cards=not_entered_cards)
@@ -36,6 +37,7 @@ def home():
 
 @app.route('/', methods=['GET', 'POST'])
 def ma():
+    print("hey!")
     return flask.render_template('main.html')
    
     
@@ -53,16 +55,51 @@ def host():
             message = "Submitted Cards!"
         else:
             # readyForm
-            code = flask.request.form['code']
-            return flask.redirect(flask.url_for('game', code=code))
-    return flask.render_template('host.html', gamecode=code,entered_code=entered_code)
+            code = flask.request.form['code2']
+            return flask.redirect(flask.url_for('game', codex=code))
+    return flask.render_template('host.html', gamecode=code,entered_code=entered_code,gamecode2=code)
     
     
-@app.route('/game/<code>', methods=['GET', 'POST'])
-def game(code):
-    code="sosat"
-    fo = open(f"/tmp/{code}.txt", "r")
+@app.route('/game/<codex>', methods=['GET', 'POST'])
+def game(codex):
+    fo = open(f"/tmp/{codex}.txt", "r")
     cards=fo.readlines()
     fo.close()
-    cards = ' '.join(cards)
-    return flask.render_template('game.html', cards = cards)
+    # cards2 = ' '.join(cards)
+    render_start = True
+    round_ended = False
+    nextnum = 0
+    cur_card = ''
+    rd = ''
+    if flask.request.method == 'POST':
+        render_start = False
+        if flask.request.form["state"] == "start":
+            # start
+            rd = list(np.arange(len(cards)))
+            shuffle(rd)
+            nextnum = 0
+            cur_card = cards[rd[0]]
+        elif flask.request.form["state"] == "next":
+            if flask.request.form["posttype"] == "nxt":
+                #next number
+                nextnum = int(flask.request.form["num"])
+                nextnum += 1
+                if nextnum == len(cards):
+                    render_start = True
+                    pass
+                else:
+                    rd = flask.request.form["rd"][1:-1]
+                    rd = [int(elem) for elem in rd.split(',')]
+                    cur_card = cards[rd[nextnum]]
+            else:
+                # prev number
+                nextnum = int(flask.request.form["num"]) - 1
+                cur_card = "..."
+                rd = flask.request.form["rd"][1:-1]
+                rd = [int(elem) for elem in rd.split(',')]
+                rd_part = rd[nextnum+1:]
+                print(f"rd:{rd}, rd_part:{rd_part}")
+                shuffle(rd_part)
+                rd[nextnum+1:] = rd_part
+        
+    return flask.render_template('game.html', nextnum = nextnum, render_start = render_start, cur_card = cur_card, rd = rd)
